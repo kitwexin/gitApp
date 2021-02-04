1a99f6cb528dd9c451523303cdcea6a6f9cc5fd6
Aleksey <kitwexin@gmail.com>
2021-02-04 14:22:39 +0300

    code 1

--- gitApp/ViewController.swift
+++ gitApp/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // code 1
+        // code 1
+        // code 1
     }
 
 
